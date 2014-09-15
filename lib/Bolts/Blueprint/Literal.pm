117cb108a40752754e25e1696f6e5abf3b045fc1
Sterling Hanenkamp <hanenkamp@cpan.org>
2014-09-15 09:11:11 -0500

    finishing up adding pod to blueprints

--- lib/Bolts/Blueprint/Literal.pm
+++ lib/Bolts/Blueprint/Literal.pm
@@ -1,20 +1,82 @@
 package Bolts::Blueprint::Literal;
 
+# ABSTRACT: A blueprint that points to a literal value
+
 use Moose;
 
 with 'Bolts::Blueprint::Role::Injector';
 
 use Carp ();
 
+=head1 SYNOPSIS
+
+    use Bolts;
+
+    # The usual sugar
+    artifact thing1 => 42;
+    artifact thing2 => ( value => 42 );
+    artifact thing3 => (
+        value => [ 'this', 'is', 'an', 'example' ],
+    );
+
+    # Or directly...
+    my $meta = Bolts::Bag->start_bag;
+
+    my $artifact = Bolts::Artifact->new(
+        name      => 'thing',
+        blueprint => $meta->locator->acquire('blueprint', 'literal', {
+            value => 42,
+        },
+        scope     => $meta->locator->acquire('scope', '_'),
+    );
+
+=head1 DESCRIPTION
+
+Provides a blueprint that points to a single value. This is best used for scalars, strings, and numbers, but could be used for references. 
+
+B<Caveat.> In the case of references, the same reference is returned every time so the contents of that reference might be modified by anyone that acquires it. This may be desirable in your application, but there's the warning in case it is not.
+
+=head1 ROLES
+
+=over
+
+=item *
+
+L<Bolts::Blueprint::Role::Injector>
+
+=back
+
+=head1 ATTRIBUTES
+
+=head2 value
+
+This is the literal value to return when this blueprint is resolved. This can be anything you can assign to a scalar variable, including references to arrays and hash (see the caveat above).
+
+=cut
+
 has value => (
     is          => 'ro',
     required    => 1,
 );
 
+=head1 METHODS
+
+=head2 builder
+
+Returns the L</value>.
+
+=cut
+
 sub builder {
     my ($self) = @_;
     $self->value;
 }
+
+=head2 implied_scope
+
+This is set. A literal blueprint value acts like a global singleton.
+
+=cut
 
 sub implied_scope { 'singleton' }
 
--- lib/Bolts/Blueprint/Role/Injector.pm
+++ lib/Bolts/Blueprint/Role/Injector.pm
@@ -1,7 +1,27 @@
 package Bolts::Blueprint::Role::Injector;
 
+# ABSTRACT: Tags a blueprint as being usable during injection
+
 use Moose::Role;
 
 with 'Bolts::Blueprint';
+
+=head1 DESCRIPTION
+
+This role tags a class as a blueprint that may be used for injection. The difference between a common blueprint and an injector blueprint is that the parameters passed for an injector will always be passed as a hash in list form. Whereas a regular blueprint may receive any kind of argument list. Also, the injection parameters are not as well targeted or filtered as they are during regular blueprint resolution.
+
+A blueprint may implement this instead of L<Bolts::Blueprint> if it could be useful to during injection and ignores or is able to process parameters as a hash.
+
+=head1 ROLES
+
+=over
+
+=item *
+
+L<Bolts::Blueprint>
+
+=back
+
+=cut
 
 1;
