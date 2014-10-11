6af82b95065ea096837677473bbcd79c444a449d
Sterling Hanenkamp <hanenkamp@cpan.org>
2014-10-10 21:29:09 -0500

    Give locator_for and meta_locator_for consistent return values

--- lib/Bolts/Util.pm
+++ lib/Bolts/Util.pm
@@ -8,10 +8,14 @@ use Moose::Exporter;
 use Bolts::Locator;
 use Moose::Util;
 use Safe::Isa;
+use Hash::Util::FieldHash 'fieldhash';
 
 Moose::Exporter->setup_import_methods(
     as_is => [ qw( locator_for meta_locator_for ) ],
 );
+
+fieldhash my %locator;
+fieldhash my %meta_locator;
 
 =head1 SYNOPSIS
 
@@ -36,7 +40,7 @@ This provides some helpful utility methods for use with Bolts.
 
     my $loc = locator_for($bag);
 
-Given a bag, it will return a L<Bolts::Role::Locator> for acquiring artifacts from it. If the bag provides it's own locator, the bag will be returned. If it doesn't (e.g., if it's a hash or an array), then a new locator will be built to locate within the bag and returned.
+Given a bag, it will return a L<Bolts::Role::Locator> for acquiring artifacts from it. If the bag provides it's own locator, the bag will be returned. If it doesn't (e.g., if it's a hash or an array or just some other object that doesn't have a locator built-in), then a new locator will be built to locate within the bag and returned on the first call. Subsequent calls using the same reference will return the same locator object.
 
 =cut
 
@@ -46,8 +50,11 @@ sub locator_for {
     if ($bag->$_does('Bolts::Role::Locator')) {
         return $bag;
     }
+    elsif (defined $locator{ $bag }) {
+        return $locator{ $bag };
+    }
     else {
-        return Bolts::Locator->new($bag);
+        return $locator{ $bag } = Bolts::Locator->new($bag);
     }
 }
 
@@ -55,7 +62,7 @@ sub locator_for {
 
     my $metaloc = meta_locator_for($bag);
 
-Attempts to find the meta locator for the bag. It returns a L<Bolts::Role::Locator> that is able to return artifacts used to manage a collection of bolts bags and artifacts. If the bag itself does not have such a locator associated with it, one is constructed using the L<Bolts/$Bolts::GLOBAL_FALLBACK_META_LOCATOR> class, which is L<Bolts::Meta::Locator> by default.
+Attempts to find the meta locator for the bag. It returns a L<Bolts::Role::Locator> that is able to return artifacts used to manage a collection of bolts bags and artifacts. If the bag itself does not have such a locator associated with it, one is constructed using the L<Bolts/$Bolts::GLOBAL_FALLBACK_META_LOCATOR> class, which is L<Bolts::Meta::Locator> by default. After the first call, the object created the first time for each reference will be reused.
 
 =cut
 
@@ -70,7 +77,11 @@ sub meta_locator_for {
         }
     }
 
-    return $Bolts::GLOBAL_FALLBACK_META_LOCATOR->new;
+    elsif (defined $meta_locator{ $bag }) {
+        return $meta_locator{ $bag };
+    }
+
+    return $meta_locator{ $bag } = $Bolts::GLOBAL_FALLBACK_META_LOCATOR->new;
 }
 
 1;
